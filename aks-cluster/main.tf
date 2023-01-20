0505bfeafecee8f775304ecfae5f68d44e12b726
Yogesh0777 <94049153+Yogesh0777@users.noreply.github.com>
2023-01-20 08:44:12 +0530

    Update main.tf

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -5,15 +5,16 @@
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
+  kubernetes_version  = var.kubernetes_version  #check the k8s version is available or not.
+                                                # az aks get-versions --location <location-name>
   location = var.location
   resource_group_name = azurerm_resource_group.aks-rg.name
   dns_prefix = var.cluster_name
 
   default_node_pool {
     name = "vm"
-    node_count = var.node_count
-    vm_size = "Standard_B2s"
+    node_count = var.node_count # use node according to your need
+    vm_size = "Standard_B2s" # use vm size according to your need.
   }
 
   identity {
